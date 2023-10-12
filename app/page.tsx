5af0a41cc7f7334ac4bc9a97f7b2981b2d8102bf
Douglas <yogi.yokomizo@gmail.com>
2023-10-12 08:39:23 -0300

    feat: add new button

--- app/page.tsx
+++ app/page.tsx
@@ -8,11 +8,18 @@ export default function Home() {
         Experimente o sabor da culinária japonesa
       </h3>
       <p className="mt-16 mb-20 text-3xl w-1/2">Sinta o sabor das comidas mais populares da cultura japonesa em qualquer lugar e à qualquer hora </p>
-      <Link href='/Menu' >
-        <button className="bg-black font-bold text-white py-4 px-10 rounded-[50px] transition ease-in-out delay-100 hover:-translate-y-1 hover:scale-110 duration-300">
-          Peça agora
-        </button>
-      </Link>
+      <div className="flex gap-8">
+        <Link href='/Menu' >
+          <button className="bg-black font-bold text-white py-4 px-10 rounded-[50px] transition ease-in-out delay-100 hover:-translate-y-1 hover:scale-110 duration-300">
+            Peça agora
+          </button>
+        </Link>
+        <Link href='About'>
+          <button className="font-bold text-black border border-black py-4 px-10 rounded-[50px] transition ease-in-out delay-100 hover:-translate-y-1 hover:scale-110 duration-300">
+            Saiba mais
+          </button>
+        </Link>
+      </div>
     </div>
   )
 }
